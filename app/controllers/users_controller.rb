0fd72adbe2d8e15a45e0ca629586158f4c8d0518
kenta-h <kenta725jp@yahoo.co.jp>
2021-04-25 21:52:34 +0900

    ユーザー詳細機能、ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+
+    def show
+        @users = User.find(params[:id])
+    end
     
     def new
         @user = User.new
